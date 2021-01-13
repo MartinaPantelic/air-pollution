930500a33947f62e94dd7495db0d47fbd42e122c
Martina <martinapantelic360@gmail.com>
2021-01-12 18:23:21 -0600

    sign out added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react"
+import { Button } from "react-bootstrap"
 
-import {
-  Link
-} from 'react-router-dom';
+import { useAuth } from "../context/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+
 
 
 
 const Navigation = props => {
-  console.log("menu r")
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      setError("Failed to log out")
+    }
+  }
   return (
 
     <div className="menu-items">
@@ -19,6 +33,10 @@ const Navigation = props => {
         <li><Link className="menu-link" exact="true" to="/login">Log In</Link></li>
         <li><Link className="menu-link" exact="true" to="/signup">Sign Up</Link></li>
         <li><Link className="menu-link" exact="true" to="/forgot-password">Forgot Pass</Link></li>
+        <li>  <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+</li>
       </ul>
     </div>
 
