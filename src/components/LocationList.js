6edc6ba1fde5941b7b004f1e8da73d7ce2368599
Martina <martinapantelic360@gmail.com>
2021-01-19 14:28:02 -0600

    delete button added

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -7,10 +7,13 @@ const LocationList = props => {
       <h3 className="locations-title mb-3">My Locations</h3>
       <ul>
         {props.location.map(locationItem => (
-          <li key={locationItem.id} onClick={props.onRemoveItem.bind(this, locationItem.id)}> 
-          <a className="list-group-item list-group-item-action">{locationItem.place}</a>
-            <span>{locationItem.lon}</span>
-            <span>{locationItem.lat}</span>
+          <li key={locationItem.id} className="d-flex list-group-item list-group-item-action"> 
+          <a className="custom-tooltip">{locationItem.place}
+          <span class="tooltiptext">Click to see Air Data for {locationItem.place}</span>
+          </a>
+            <span className="d-none">{locationItem.lon}</span>
+            <span className="d-none">{locationItem.lat}</span>
+            <span onClick={props.onRemoveItem.bind(this, locationItem.id)} className="delete-icon"></span>
           </li>
         ))}
       </ul>
